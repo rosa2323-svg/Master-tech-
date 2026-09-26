b0f8a873b89152978938d387072f4a66b60f1067
Sebas236-em <239686239+Sebas236-em@users.noreply.github.com>
2026-09-26 02:30:04 +0000

    chore: auto-update database schema backup

--- supabase/migrations/esquema_actualizado.sql
+++ supabase/migrations/esquema_actualizado.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict dmOBqbGihcCDEgYj0qVm2FtbxuiDSp8SoXaMdMPTKhczuSgYgqmbVhf1LjuyD0T
+\restrict kt41AopshZEamYwuKI8AE9g9RpVzqTGjsa7vl5r4WNuRs9qViV5GVe21ERyAfwz
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.11 (Ubuntu 17.11-1.pgdg24.04+2)
@@ -6382,5 +6382,5 @@ CREATE EVENT TRIGGER pgrst_drop_watch ON sql_drop
 -- PostgreSQL database dump complete
 --
 
-\unrestrict dmOBqbGihcCDEgYj0qVm2FtbxuiDSp8SoXaMdMPTKhczuSgYgqmbVhf1LjuyD0T
+\unrestrict kt41AopshZEamYwuKI8AE9g9RpVzqTGjsa7vl5r4WNuRs9qViV5GVe21ERyAfwz
 
